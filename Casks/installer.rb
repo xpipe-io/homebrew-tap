38a70a0da9bcba0ece6b3ab48d85d49b6f9dae9a
jreleaserbot <jreleaser@kordamp.org>
2023-04-29 21:12:17 +0200

    installer 0.5.38

---
+++ Casks/installer.rb
@@ -0,0 +1,14 @@
+# Generated with JReleaser 1.6.0 at 2023-04-29T21:12:16.611049+02:00
+cask "installer" do
+  desc "A brand-new type of connection manager and remote file explorer."
+  homepage "https://github.com/xpipe-io/xpipe"
+  url "https://github.com/xpipe-io/xpipe/releases/download/0.5.38/xpipe-installer-macos-x86_64.pkg",
+      verified: "github.com"
+  version "0.5.38"
+  sha256 "cb7dc42840c030771a006cba95d89654f3125080416b444241c2e199d4cbe54e"
+  name "Installer"
+  auto_updates true
+
+
+  pkg "xpipe-installer-macos-x86_64.pkg"
+end
