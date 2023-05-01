1fa0af450e1400c2c1775f7cd8d13ea87c741088
crschnick <crschnick@xpipe.io>
2023-05-01 11:08:21 +0200

    0.5.38

--- Casks/xpipe.rb
+++ Casks/xpipe.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.6.0 at 2023-05-01T09:27:02.322115+02:00
+# Generated with JReleaser 1.6.0 at 2023-05-01T11:08:20.788293+02:00
 cask "xpipe" do
   desc "A brand-new type of connection manager and remote file explorer."
   homepage "https://github.com/xpipe-io/xpipe"
@@ -6,12 +6,15 @@ cask "xpipe" do
       verified: "github.com"
   version "0.5.38"
   sha256 "cb7dc42840c030771a006cba95d89654f3125080416b444241c2e199d4cbe54e"
-  name "Xpipe"
+  name "X-Pipe"
   auto_updates true
 
 
   pkg "xpipe-installer-macos-x86_64.pkg"
-  uninstall pkgutil: [
-    "io.xpipe.xpipe",
-  ]
+  uninstall script:  {
+        executable: "/Applications/X-Pipe.app/Contents/Resources/scripts/uninstall.sh",
+        args:       [],
+        sudo:       true,
+      },
+      pkgutil: "io.xpipe.xpipe"
 end
