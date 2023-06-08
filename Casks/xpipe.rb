363f666ea2d7d10abfafe4a077801f91d06b3396
crschnick <crschnick@xpipe.io>
2023-06-08 17:32:02 +0000

    1.1.1

--- Casks/xpipe.rb
+++ Casks/xpipe.rb
@@ -1,6 +1,6 @@
 cask "xpipe" do
   arch arm: "arm64", intel: "x86_64"
-  version "1.1.0"
+  version "1.1.1"
   desc "A brand-new type of connection manager and remote file explorer."
   homepage "https://xpipe.io"
   url "https://github.com/xpipe-io/xpipe/releases/download/#{version}/xpipe-installer-macos-#{arch}.pkg",
