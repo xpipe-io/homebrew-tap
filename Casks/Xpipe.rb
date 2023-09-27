a188515529686a4d01cb7ac7b57f3606f3a09fa0
crschnick <crschnick@xpipe.io>
2023-09-27 03:57:15 +0000

    1.6.0

---
+++ Casks/Xpipe.rb
@@ -0,0 +1,29 @@
+cask "Xpipe" do
+  arch arm: "arm64", intel: "x86_64"
+  version "1.6.0"
+  desc "A brand-new type of connection manager and remote file explorer."
+  homepage "https://xpipe.io"
+  url "https://github.com/xpipe-io/xpipe/releases/download/#{version}/xpipe-installer-macos-#{arch}.pkg",
+      verified: "github.com"
+  # We can't verify the checksum until both arch builds are done on CI
+  sha256 :no_check
+  name "XPipe"
+  auto_updates true
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+
+  pkg "xpipe-installer-macos-#{arch}.pkg"
+  uninstall script:  {
+        executable: "/Applications/XPipe.app/Contents/Resources/scripts/uninstall.sh",
+        args:       [],
+        sudo:       true,
+      },
+      pkgutil: "io.xpipe.xpipe"
+  zap trash: [
+    "~/.xpipe"
+  ]
+end
