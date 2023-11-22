6e33a726f94f7fe8d7979dffe8fdd4e20d495506
crschnick <crschnick@xpipe.io>
2023-11-22 07:11:55 +0000

    1.7.7

--- Casks/Xpipe.rb
+++ Casks/Xpipe.rb
@@ -1,6 +1,6 @@
 cask "xpipe" do
   arch arm: "arm64", intel: "x86_64"
-  version "1.7.6"
+  version "1.7.7"
   desc "Your entire server infrastructure at your fingertips"
   homepage "https://xpipe.io"
   url "https://github.com/xpipe-io/xpipe/releases/download/#{version}/xpipe-installer-macos-#{arch}.pkg",
