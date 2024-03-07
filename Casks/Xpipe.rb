0dcfe622f1c0cc3b1cf8db5c0d844d1e3de018fd
crschnick <crschnick@xpipe.io>
2024-03-07 19:59:09 +0000

    8.2

--- Casks/Xpipe.rb
+++ Casks/Xpipe.rb
@@ -1,6 +1,6 @@
 cask "xpipe" do
   arch arm: "arm64", intel: "x86_64"
-  version "8.1"
+  version "8.2"
   desc "Your entire server infrastructure at your fingertips"
   homepage "https://xpipe.io"
   url "https://github.com/xpipe-io/xpipe/releases/download/#{version}/xpipe-installer-macos-#{arch}.pkg",
