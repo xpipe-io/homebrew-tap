a72f3cb7f4938a1a2cafb180d64b423863bb4b55
Christopher Schnick <crschnick@xpipe.io>
2025-08-04 17:59:03 +0000

    18.0-6-SNAPSHOT

--- Casks/xpipe.rb
+++ Casks/xpipe.rb
@@ -1,29 +1,14 @@
-cask "xpipe" do
-  arch arm: "arm64", intel: "x86_64"
-  version "17.4"
-  desc "Your entire server infrastructure at your fingertips"
-  homepage "https://xpipe.io"
-  url "https://github.com/xpipe-io/xpipe/releases/download/#{version}/xpipe-installer-macos-#{arch}.pkg",
-      verified: "github.com"
-  sha256 arm: "c9856a8fea3b7867859d0f18b6f85d38d5275489c181d1a572d5476bfe1b2e7c", intel: "4efcc5e76b02c46f80a8873bd5b5ef9db03d258c12f6ab5657b4f2591879d5ef"
-  name "XPipe"
-  auto_updates true
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
+$packageArgs = @{
+  packageName   = 'xpipe'
+  fileType      = 'msi'
+  url           = 'https://github.com/xpipe-io/xpipe/releases/download/18.0-6-SNAPSHOT/xpipe-installer-windows-x86_64.msi'
+  silentArgs    = "/quiet"
+  validExitCodes= @(0)
+  softwareName  = 'xpipe*'
+  checksum      = '013867c733e632e618695f97368d54b6311cb4c5298bd65fb7db7933c229ef53'
+  checksumType  = 'sha256'
+}
 
-  depends_on formula: "util-linux"
-
-  pkg "xpipe-installer-macos-#{arch}.pkg"
-  uninstall script:  {
-        executable: "/Applications/XPipe.app/Contents/Resources/scripts/uninstall.sh",
-        args:       [],
-        sudo:       true,
-      },
-      pkgutil: "io.xpipe.xpipe"
-  zap trash: [
-    "~/.xpipe"
-  ]
-end
+Install-ChocolateyPackage @packageArgs
