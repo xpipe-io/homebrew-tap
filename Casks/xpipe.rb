5c2792b409c46f2b14ab33b41e958e9dc8414a18
Christopher Schnick <crschnick@xpipe.io>
2026-09-04 09:24:39 +0000

    24.0.1

--- Casks/xpipe.rb
+++ Casks/xpipe.rb
@@ -1,11 +1,11 @@
 cask "xpipe" do
   arch arm: "arm64", intel: "x86_64"
-  version "24.0"
+  version "24.0.1"
   desc "Your entire server infrastructure at your fingertips"
   homepage "https://xpipe.io"
   url "https://github.com/xpipe-io/xpipe/releases/download/#{version}/xpipe-installer-macos-#{arch}.pkg",
       verified: "github.com"
-  sha256 arm: "a1653774c598d5551ccf340f7b1a8fb2508bd3f61f1d626ec2562f1ea2a2c4f3", intel: "c03b02b2f5b3cf87e77c2ddfea3649b88dafe690c338314a34a9d441f8fdeb0e"
+  sha256 arm: "ef5ce8cf11595384dc62c9f0d2727bd0c6a16b1b2a0a505255ea4e2fd1462435", intel: "c3596cb226c0f8193676ec55bcd3dba4b47b6c78773a41b9bdab4e1ccda67598"
   name "XPipe"
   auto_updates true
 
